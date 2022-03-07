95c6b4fe77a38a7223cea11429520e0552fb3fa9
jeongabae <gabea52@gmail.com>
2022-03-07 23:49:00 +0900

    [create] 부녀회장이 될테야 baekjoon 3/6

---
+++ bj2775.py
@@ -0,0 +1,9 @@
+T = int(input())
+for _ in range(T):
+    floor = int(input())
+    unit = int(input())
+    apart =[i for i in range(1, unit + 1)] #0층
+    for j in range(floor):
+        for k in range(1,unit):
+            apart[k] += apart[k - 1]
+    print(apart[-1])
