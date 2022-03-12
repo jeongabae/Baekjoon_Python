18b86cfc2cb941b76ff75ac65a9a721bd06bedea
jeongabae <gabea52@gmail.com>
2022-03-12 23:18:43 +0900

    [create] 영화감독 숌 baekjoon 3/12

---
+++ bj1436.py
@@ -0,0 +1,10 @@
+n = int(input())
+cnt = 0
+movie_num = 666
+while 1:
+    if '666' in str(movie_num):
+        cnt += 1
+    if cnt == n:
+        print(movie_num)
+        break
+    movie_num += 1
