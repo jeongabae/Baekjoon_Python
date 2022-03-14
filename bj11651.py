549e504eacccf0312e6cf4502e693d1e0eff0771
jeongabae <gabea52@gmail.com>
2022-03-14 23:46:48 +0900

    [create] 좌표 정렬하기2 baekjoon 3/14

--- bj11651.py
+++ bj11651.py
@@ -3,3 +3,14 @@ xy=[list(map(int,input().split())) for i in range(N)]
 xy.sort(key=lambda x: (x[1], x[0]))
 for i in xy:
     print(i[0], i[1])
+"""다른 사람 코드 
+num = int(input())
+a = []
+for i in range(num):
+    [x, y] = map(int, input().split())
+    rev = [y, x]
+    a.append(rev)
+b = sorted(a)
+for i in range(num):
+    print(b[i][1], b[i][0])
+    """
