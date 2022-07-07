d3f728d5c754d41b70b5147a43858bf33e3dc1a9
jeongabae <gabea52@gmail.com>
2022-07-07 21:27:17 +0900

    [create] math

--- python_1~50/bj1934.py
+++ python_1~50/bj1934.py
@@ -2,7 +2,7 @@ def GCD(a,b):
     while b!=0:
         r = a%b
         a = b
-        b = r
+        b = r #세 줄을 a, b = b, a%b로 줄여도 됨.
     return a
 
 def LCM(a,b):
@@ -12,7 +12,10 @@ t = int(input())
 for _ in range(t):
     a, b = map(int,input().split())
     print(LCM(a,b))
-
+"""cf) 최소공약수 구하는 함수 있음!!
+import math
+a = math.gcd(24, 8)
+"""
 #     #1등 코드
 # import math
 # import sys
