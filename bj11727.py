f01513eb4b3eff6f663c903dd0f1306e388fbe22
jeongabae <gabea52@gmail.com>
2022-07-11 14:21:17 +0900

    [create] 2×n 타일링 2 (Dynamic Programming)

---
+++ bj11727.py
@@ -0,0 +1,8 @@
+import sys
+N = int(sys.stdin.readline().rstrip())
+d = [0, 1, 3] + [0] * (N - 2)
+for i in range(3,N+1):
+    d[i] = d[i - 1] + 2*d[i - 2]
+print(d[N] % 10007)
+# n=2일 때  2*1타일을 | 1*2 타일 두 개 놓은 것을 = 2*2타일을 ㅁ이라고하면
+# ||, =, ㅁ이므로 총 3
