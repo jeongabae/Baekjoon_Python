55dc6ab379a0b8bbb77c75a1cf51c874047878b3
jeongabae <gabea52@gmail.com>
2022-08-16 00:11:52 +0900

    [create]

--- 10000~10499/bj10093.py
+++ 10000~10499/bj10093.py
@@ -1,12 +1,12 @@
 # 백준 10093번 : 숫자 : 구현
 a, b = map(int, input().split())
-a2 = min(a, b)
-b2 = max(a, b)
-n = b2-a2-1
-
-if b2-a2<=1:
-    n=0
-
-print(n)
+a2, b2 = min(a, b), max(a, b)
 ans = [i for i in range(a2+1, b2)]
+print(len(ans))
 print(*ans)
+""" 더 좋은 풀이
+A,B=map(int,input().split())
+A,B=min(A,B),max(A,B)
+print(len(range(A+1,B)))
+print(' '.join(map(str,range(A+1,B))))
+"""
